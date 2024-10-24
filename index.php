eab9c44cec76a1f7b3f1504dca9237d483a8826f
Hugo Culot <hugoculot.dev@gmail.com>
2024-10-24 22:15:00 +0200

    Added color support to the text-based adventure game using ANSI codes.

--- index.php
+++ index.php
@@ -1,46 +1,51 @@
 <?php
 
+// Pour lancer le script (le jeu) faire php index.php dans le terminal.
+
+$couleurs = [
+  'rouge' => "\033[31m",
+  'vert' => "\033[32m",
+  'jaune' => "\033[33m",
+  'bleu' => "\033[34m",
+  'reset' => "\033[0m",
+];
+
 $json = file_get_contents('aventure.json');
 $aventure = json_decode($json, true);
 
-function jouerScene($sceneId) {
+function jouerScene($sceneId, $couleurs) {
   global $aventure;
 
   $scene = $aventure['scenes'][$sceneId];
-  echo $scene['text'] . "\n\n";
+  echo $couleurs['bleu'] . $scene['text'] . $couleurs['reset'] . "\n\n";
 
   if (isset($scene['end']) && $scene['end'] === true) {
-    echo "L'aventure est terminée.\n";
+    echo $couleurs['rouge'] . "L'aventure est terminée." . $couleurs['reset'] . "\n";
     return;
   }
 
   if (isset($scene['next_scene'])) {
-    jouerScene($scene['next_scene']);
+    jouerScene($scene['next_scene'], $couleurs);
   } elseif (isset($scene['options'])) {
     $options = array_keys($scene['options']);
 
     for ($i = 0; $i < count($options); $i++) {
-      echo ($i + 1) . ". " . ucfirst($options[$i]) . "\n";
+      echo $couleurs['vert'] . ($i + 1) . ". " . ucfirst($options[$i]) . $couleurs['reset'] . "\n";
     }
 
-    echo "\nFais ton choix (1 à " . count($options) . ") ou tape 'exit' pour quitter : ";
+    echo "\nFais ton choix (1 à " . count($options) . ") : ";
     $choixUtilisateur = trim(fgets(STDIN));
 
-    if (strtolower($choixUtilisateur) === 'exit') {
-      echo "Tu as quitté le jeu.\n";
-      exit;
-    }
-
     if (!is_numeric($choixUtilisateur) || $choixUtilisateur < 1 || $choixUtilisateur > count($options)) {
-      echo "Choix invalide, essaie encore.\n";
-      jouerScene($sceneId);
+      echo $couleurs['rouge'] . "Choix invalide, essaie encore." . $couleurs['reset'] . "\n";
+      jouerScene($sceneId, $couleurs);
     } else {
       $choixSuivant = $options[$choixUtilisateur - 1];
-      jouerScene($scene['options'][$choixSuivant]);
+      jouerScene($scene['options'][$choixSuivant], $couleurs);
     }
   }
 }
 
-jouerScene('start');
+jouerScene('start', $couleurs);
 
 ?>
