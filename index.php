062a0b81e065834af47d3fff7106cec608fe4b8f
Hugo Culot <hugoculot.dev@gmail.com>
2024-10-24 13:05:45 +0200

    Feature: Implementation of a text adventure in scene format.

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
 <?php
-  // Mettre le code ici :)
-?>Ò
+
+$json = file_get_contents('aventure.json');
+$aventure = json_decode($json, true);
+
+function jouerScene($sceneId) {
+  global $aventure;
+
+  $scene = $aventure['scenes'][$sceneId];
+  echo $scene['text'] . "\n\n";
+
+  if (isset($scene['end']) && $scene['end'] === true) {
+    echo "L'aventure est terminée.\n";
+    return;
+  }
+
+  if (isset($scene['next_scene'])) {
+    jouerScene($scene['next_scene']);
+  } elseif (isset($scene['options'])) {
+    $options = array_keys($scene['options']);
+
+    for ($i = 0; $i < count($options); $i++) {
+      echo ($i + 1) . ". " . ucfirst($options[$i]) . "\n";
+    }
+
+    echo "\nFais ton choix (1 à " . count($options) . ") : ";
+    $choixUtilisateur = trim(fgets(STDIN));
+
+    if (!is_numeric($choixUtilisateur) || $choixUtilisateur < 1 || $choixUtilisateur > count($options)) {
+      echo "Choix invalide, essaie encore.\n";
+      jouerScene($sceneId);
+    } else {
+      $choixSuivant = $options[$choixUtilisateur - 1];
+      jouerScene($scene['options'][$choixSuivant]);
+    }
+  }
+}
+
+jouerScene('start');
+
+?>
